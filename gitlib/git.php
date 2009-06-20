e466822928f495e61609b09612d97c0c779c7045
Dan Hackney <chrono325@gmail.com>
2009-06-20 07:57:30 -0400

    Move fast-import work to its own class.
    It is not really tester specific, so it belongs within its own file.
    
    This could also be generally useful to PHP programs in general, not just
    versioncontrol_git, so begin working it towards a generally useful 'gitlib'.
    
    Signed-off-by: Dan Hackney <chrono325@gmail.com>

---
+++ gitlib/git.php
@@ -0,0 +1,13 @@
+<?php
+// $Id$
+
+/**
+ * @file
+ * Master file for Git PHP library
+ *
+ * Loads the other required library files.
+ *
+ * Copyright 2009 by Daniel Hackney ("chrono325", http://drupal.org/user/384635)
+ */
+
+require_once dirname(__FILE__) . '/fast_import.inc';
